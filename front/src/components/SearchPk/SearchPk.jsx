b5ed79a7a63fc7f5d280616f01bbc0faf034f976
Sebastian <seborbon@outlook.com>
2021-11-27 02:54:09 -0500

    add validations to add button

--- front/src/components/SearchPk/SearchPk.jsx
+++ front/src/components/SearchPk/SearchPk.jsx
@@ -1,11 +1,32 @@
 import "./SearchPk.css";
 import { pokemonAdd } from "../../redux/actions/sending";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const SearchPk = ({ pokemonName, pokeId, sprite }) => {
   //should add a validation that don't dispatch if the pokemon already exists
   const dispatch = useDispatch();
   let userId = window.localStorage.getItem("userId");
+  const teamPokemons = useSelector((state) => state.team.team);
+
+  const pokemonExist = (pokeId) => {
+    let exist = teamPokemons.findIndex((team) => team.pokeId === pokeId);
+    return exist === -1 ? false : true;
+  };
+
+  const renderPokemons = (e) => {
+    if (!pokemonExist(pokeId)) {
+      if (teamPokemons.length === 6) {
+        toast.dark("already have  6 pokemons");
+      } else {
+        e.preventDefault();
+        dispatch(pokemonAdd(userId, pokemonName));
+      }
+    } else {
+      toast.dark("this pokemon  is already in the team");
+    }
+  };
 
   return (
     <div className="searchCard">
@@ -17,15 +38,10 @@ const SearchPk = ({ pokemonName, pokeId, sprite }) => {
         <div className="imgContainerPkSearched">
           <img className="imgSearched" src={sprite} alt="no sprite" />
         </div>
-        <button
-          className="addPokemonBtn"
-          onClick={(e) => {
-            e.preventDefault();
-            dispatch(pokemonAdd(userId, pokemonName));
-          }}
-        >
+        <button className="addPokemonBtn" onClick={(e) => renderPokemons(e)}>
           Catch!
         </button>
+        <ToastContainer />
       </div>
     </div>
   );
