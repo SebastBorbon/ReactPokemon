92e3dd0358ef8abc50e80546c29ceeb9f3c55c94
Sebastian <seborbon@outlook.com>
2021-12-01 15:30:25 -0500

    small changes

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,9 @@
 const jwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
 
 // the passport strategy is used to create tokens
-module.exports = (passport) => {
+module.exports = () => {
   const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
     secretOrKey: "secret", // ToDo should be in enviroment var
--- front/src/index.js
+++ front/src/index.js
@@ -17,8 +17,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-//this need to work implement redux
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
